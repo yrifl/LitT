33b64653961413ed3584f2beaf8b36717151a3eb
yrifl <79893882+yrifl@users.noreply.github.com>
2023-04-05 01:26:08 +0700

    Ready, Go!

--- src/Temp/dump/Slug.lua
+++ src/Temp/dump/Slug.lua
@@ -1,18 +1,15 @@
-local M = {
-
-}
+local M = {}
 
 function M.Log(Head, Info)
-	print("[ ".. Head .." ]  :-> ".. Info)
+	print("[ " .. Head .. " ]  :-> " .. Info)
 end
 
 function M.Warn(Head, Info)
-	warn("[ ".. Head .." ]  :-> ".. Info)
+	warn("[ " .. Head .. " ]  :-> " .. Info)
 end
 
 function M.Error(Head, Info)
-	error("[ ".. Head .." ]  :-> ".. Info)
+	error("[ " .. Head .. " ]  :-> " .. Info)
 end
-
 
 return M
